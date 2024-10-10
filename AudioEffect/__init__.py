0b449d92eced9ecb3c6c054d9c00ffc42207dc62
SappirBo <sappirbohbot@gmail.com>
2024-10-10 15:10:17 +0300

    addig auto FX read

--- AudioEffect/__init__.py
+++ AudioEffect/__init__.py
@@ -2,7 +2,7 @@ import json
 from .EffectInterface import EffectInterface
 from .DigitalDelay import DigitalDelay
 from .Overdrive import Overdrive
-
+import os
 
 def updateEffectsJson():
     effects_list = getEffectsList()
@@ -11,7 +11,7 @@ def updateEffectsJson():
         json.dump(effects_dict, f)
     
 def getEffectsList() -> list[str]:
-    effects_list = [
+    effects_list_tmp = [
         "Sappir",
         "BB Tubes",
         "API 2500",
@@ -29,8 +29,32 @@ def getEffectsList() -> list[str]:
         "CLA-76 Compressor / Limiter"
     ]
 
+
+    effects_list = res = get_all_files_in_this_dir()
+    map_out_interface(res)
+    remove_py_suffix(res)
+
     return effects_list
 
-#ToDo: Read the current avaliable effects!
+def get_all_files_in_this_dir() -> list:
+    
+    dir_path = os.path.dirname(os.path.realpath(__file__)) # folder path
+    res = [] # list to store files
+
+    for path in os.listdir(dir_path):
+        if os.path.isfile(os.path.join(dir_path, path)): # check if current path is a file
+            res.append(path)
+    print(res)
+    return res
+
+def map_out_interface(files_list:list) -> None:
+    files_list.remove("EffectInterface.py")
+    files_list.remove("__init__.py")
+
+def remove_py_suffix(files_list: list) -> None:
+    for i in range(len(files_list)):
+        if files_list[i].endswith('.py'):
+            files_list[i] = files_list[i][:-3]
+
 
 updateEffectsJson()
